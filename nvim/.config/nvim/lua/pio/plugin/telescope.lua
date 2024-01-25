be368eb296236bb0d19d69fcc9c4449f5d585460
xinoip <xinoip@proton.me>
2024-01-25 21:11:22 +0300

    nvim: add dressing, update telescope

--- nvim/.config/nvim/lua/pio/plugin/telescope.lua
+++ nvim/.config/nvim/lua/pio/plugin/telescope.lua
@@ -1,16 +1,45 @@
 return {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.4',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    branch = '0.1.x',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            build =
+            'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+        },
+        'nvim-tree/nvim-web-devicons',
+    },
     config = function()
         local telescope = require('telescope')
-        telescope.setup {
+        local actions = require('telescope.actions')
+        telescope.setup({
+            extensions = {
+                fzf = {
+                    fuzzy = true,                   -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true,    -- override the file sorter
+                    case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                }
+            },
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<C-k>"] = actions.move_selection_previous,
+                        ["<C-j>"] = actions.move_selection_next,
+                    }
+                }
+            },
             pickers = {
                 find_files = {
                     hidden = true
                 }
             }
-        }
+        })
+
+        telescope.load_extension('fzf')
+
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fp', builtin.git_files, {})
