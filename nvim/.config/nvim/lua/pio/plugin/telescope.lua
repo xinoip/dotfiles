3c31fe7343c31d6c1604c1ed0be75d5dafd60332
xinoip <xinoip@proton.me>
2024-03-21 21:42:13 +0300

    vim: oil & telescope bindings

--- nvim/.config/nvim/lua/pio/plugin/telescope.lua
+++ nvim/.config/nvim/lua/pio/plugin/telescope.lua
@@ -42,7 +42,7 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fp', builtin.git_files, {})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
