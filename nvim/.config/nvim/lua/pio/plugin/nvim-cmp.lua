0a0bda5a012dcc1eb3846cecf01f4450992fc04a
xinoip <xinoip@proton.me>
2024-03-23 18:09:06 +0300

    nvim: lua snip bindings

--- nvim/.config/nvim/lua/pio/plugin/nvim-cmp.lua
+++ nvim/.config/nvim/lua/pio/plugin/nvim-cmp.lua
@@ -39,6 +39,16 @@ return {
                 { name = "path" },
             }
         })
+
+        vim.keymap.set({ "i" }, "<C-K>", function() luasnip.expand() end, { silent = true })
+        vim.keymap.set({ "i", "s" }, "<C-L>", function() luasnip.jump(1) end, { silent = true })
+        vim.keymap.set({ "i", "s" }, "<C-J>", function() luasnip.jump(-1) end, { silent = true })
+
+        vim.keymap.set({ "i", "s" }, "<C-E>", function()
+            if luasnip.choice_active() then
+                luasnip.change_choice(1)
+            end
+        end, { silent = true })
     end
 
 }
