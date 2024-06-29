297423f64e101e2cfb8a99eb32170d3b3bce09a8
xinoip <xinoip@proton.me>
2024-06-29 22:27:40 +0300

    nvim: add leap.nvim

---
+++ nvim/.config/nvim/lua/pio/plugin/leap.lua
@@ -0,0 +1,19 @@
+return {
+    "ggandor/leap.nvim",
+    dependencies = { "tpope/vim-repeat" },
+    config = function()
+        local leap = require("leap")
+        leap.create_default_mappings()
+
+        leap.opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
+        leap.opts.special_keys.prev_target = '<backspace>'
+        leap.opts.special_keys.prev_group = '<backspace>'
+
+        vim.keymap.set('n', 's', '<Plug>(leap)')
+        vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+        vim.keymap.set({ 'x', 'o' }, 's', '<Plug>(leap-forward)')
+        vim.keymap.set({ 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+
+        require("leap.user").set_repeat_keys('<enter>', '<backspace>')
+    end
+}
