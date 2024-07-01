33f5db06cae0906cd4b9e0e049db31079bce6636
xinoip <xinoip@proton.me>
2024-07-01 16:19:27 +0300

    nvim: fix bg

--- nvim/.config/nvim/lua/pio/init.lua
+++ nvim/.config/nvim/lua/pio/init.lua
@@ -1,3 +1,7 @@
 require('pio.vim')
 require('pio.remap')
 require('pio.lazy')
+
+-- Transparent background
+vim.cmd [[highlight Normal guibg=NONE ctermbg=NONE]]
+vim.cmd [[highlight NormalNC guibg=NONE ctermbg=NONE]]
