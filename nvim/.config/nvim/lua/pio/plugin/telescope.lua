11599f55afe8466ba78cf0313285cb66c56960bf
xinoip <xinoip@proton.me>
2024-07-06 00:13:20 +0300

    nvim: lsp symbols & trouble

--- nvim/.config/nvim/lua/pio/plugin/telescope.lua
+++ nvim/.config/nvim/lua/pio/plugin/telescope.lua
@@ -54,5 +54,6 @@ return {
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>fs', builtin.lsp_workspace_symbols, {})
     end
 }
