219199c8af302f1238d02210ef78944593b30447
xinoip <xinoip@proton.me>
2024-10-15 19:45:16 +0300

    nvim: telescope resume

--- nvim/.config/nvim/lua/pio/plugin/telescope.lua
+++ nvim/.config/nvim/lua/pio/plugin/telescope.lua
@@ -5,56 +5,55 @@ return {
         'nvim-lua/plenary.nvim',
         {
             'nvim-telescope/telescope-fzf-native.nvim',
-            build =
-            'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+            build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
         },
         'nvim-tree/nvim-web-devicons',
     },
     config = function()
-        local telescope = require('telescope')
-        local actions = require('telescope.actions')
+        local telescope = require 'telescope'
+        local actions = require 'telescope.actions'
         local ignored = { 'node_modules', '.git', '.venv' }
-        telescope.setup({
+        telescope.setup {
             extensions = {
                 fzf = {
-                    fuzzy = true,                   -- false will only do exact matching
+                    fuzzy = true, -- false will only do exact matching
                     override_generic_sorter = true, -- override the generic sorter
-                    override_file_sorter = true,    -- override the file sorter
-                    case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+                    override_file_sorter = true, -- override the file sorter
+                    case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
                     -- the default case_mode is "smart_case"
-                }
+                },
             },
             defaults = {
                 mappings = {
                     i = {
-                        ["<C-k>"] = actions.move_selection_previous,
-                        ["<C-j>"] = actions.move_selection_next,
-                    }
-                }
+                        ['<C-k>'] = actions.move_selection_previous,
+                        ['<C-j>'] = actions.move_selection_next,
+                    },
+                },
             },
             pickers = {
                 find_files = {
                     file_ignore_patterns = ignored,
-                    hidden = true
+                    hidden = true,
                 },
                 live_grep = {
                     file_ignore_patterns = ignored,
                     additional_args = function(_)
-                        return { "--hidden" }
-                    end
-                }
-            }
-        })
+                        return { '--hidden' }
+                    end,
+                },
+            },
+        }
 
-        telescope.load_extension('fzf')
+        telescope.load_extension 'fzf'
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
+        local builtin = require 'telescope.builtin'
+        vim.keymap.set('n', '<leader>ff', builtin.resume, {})
         vim.keymap.set('n', '<C-p>', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
         -- vim.keymap.set('n', '<leader>fs', builtin.lsp_workspace_symbols, {})
         -- vim.keymap.set('n', '<leader>fm', builtin.man_pages, {})
-    end
+    end,
 }
