52acd2b168dd76c3492d91a06fe278a9a721177e
xinoip <xinoip@proton.me>
2024-11-12 20:43:50 +0300

    nvim: bash script lsp, linter, formatter support

--- nvim/.config/nvim/lua/pio/plugin/lsp.lua
+++ nvim/.config/nvim/lua/pio/plugin/lsp.lua
@@ -108,6 +108,7 @@ return {
                     },
                 },
                 gopls = {},
+                bashls = {},
             }
 
             -- Let Mason install the servers, check with :Mason
@@ -118,6 +119,8 @@ return {
             vim.list_extend(ensure_installed, {
                 'stylua', -- Used to format Lua code
                 'cmakelang', -- cmake-format
+                'shfmt', -- bash-language-server formatter
+                'shellcheck', -- bash-language-server linter
             })
             require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
