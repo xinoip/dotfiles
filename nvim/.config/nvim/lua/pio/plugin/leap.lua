63ff848c3c289181b5cc06d81acdd18764939509
xinoip <xinoip@proton.me>
2024-11-12 21:45:44 +0300

    nvim: enchance leap with flit

--- nvim/.config/nvim/lua/pio/plugin/leap.lua
+++ nvim/.config/nvim/lua/pio/plugin/leap.lua
@@ -1,18 +1,27 @@
 return {
-    'ggandor/leap.nvim',
-    dependencies = { 'tpope/vim-repeat' },
-    config = function()
-        local leap = require 'leap'
+    {
+        'ggandor/leap.nvim',
+        dependencies = { 'tpope/vim-repeat' },
+        config = function()
+            local leap = require 'leap'
 
-        leap.opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
-        leap.opts.special_keys.prev_target = '<backspace>'
-        leap.opts.special_keys.prev_group = '<backspace>'
+            leap.opts.equivalence_classes = { ' \t\r\n', '([{', ')]}', '\'"`' }
+            leap.opts.special_keys.prev_target = '<backspace>'
+            leap.opts.special_keys.prev_group = '<backspace>'
 
-        vim.keymap.set('n', 'S', '<Plug>(leap)')
-        -- vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
-        vim.keymap.set({ 'x', 'o' }, 'S', '<Plug>(leap-forward)')
-        -- vim.keymap.set({ 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+            vim.keymap.set('n', 'S', '<Plug>(leap)')
+            -- vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+            vim.keymap.set({ 'x', 'o' }, 'S', '<Plug>(leap-forward)')
+            -- vim.keymap.set({ 'x', 'o' }, 'S', '<Plug>(leap-backward)')
 
-        require('leap.user').set_repeat_keys('<enter>', '<backspace>')
-    end,
+            require('leap.user').set_repeat_keys('<enter>', '<backspace>')
+        end,
+    },
+    {
+        'ggandor/flit.nvim',
+        dependencies = { 'ggandor/leap.nvim' },
+        config = function()
+            require('flit').setup()
+        end,
+    },
 }
