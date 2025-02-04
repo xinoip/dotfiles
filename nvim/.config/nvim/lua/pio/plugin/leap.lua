34102ea3a16b988643f85b4e7112e356e7f782b9
xinoip <xinoip@proton.me>
2025-02-04 22:41:21 +0300

    nvim: leap surround keymap fix

--- nvim/.config/nvim/lua/pio/plugin/leap.lua
+++ nvim/.config/nvim/lua/pio/plugin/leap.lua
@@ -9,9 +9,10 @@ return {
             leap.opts.special_keys.prev_target = '<backspace>'
             leap.opts.special_keys.prev_group = '<backspace>'
 
-            vim.keymap.set('n', 'S', '<Plug>(leap)')
+            require('leap').create_default_mappings()
+            -- vim.keymap.set('n', 's', '<Plug>(leap)')
             -- vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
-            vim.keymap.set({ 'x', 'o' }, 'S', '<Plug>(leap-forward)')
+            -- vim.keymap.set({ 'x', 'o' }, 'S', '<Plug>(leap-forward)')
             -- vim.keymap.set({ 'x', 'o' }, 'S', '<Plug>(leap-backward)')
 
             require('leap.user').set_repeat_keys('<enter>', '<backspace>')
