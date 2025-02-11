680cd46a6c746a6ea62f82b5d16fc30f57fc4691
xinoip <xinoip@proton.me>
2025-02-11 23:04:46 +0300

    nvim: telescope lsp symbols

--- nvim/.config/nvim/lua/pio/plugin/telescope.lua
+++ nvim/.config/nvim/lua/pio/plugin/telescope.lua
@@ -53,7 +53,9 @@ return {
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-        -- vim.keymap.set('n', '<leader>fs', builtin.lsp_workspace_symbols, {})
-        -- vim.keymap.set('n', '<leader>fm', builtin.man_pages, {})
+        -- vim.keymap.set('n', '<leader>fo', builtin.lsp_workspace_symbols, {})
+        -- vim.keymap.set('n', '<leader>fo', builtin.lsp_dynamic_workspace_symbols, {})
+        vim.keymap.set('n', '<leader>fo', builtin.lsp_document_symbols, {})
+        vim.keymap.set('n', '<leader>fm', builtin.man_pages, {})
     end,
 }
