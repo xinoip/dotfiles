09655dc4f181afe1bceb33b8bed6478a0fd6ac9b
xinoip <xinoip@proton.me>
2025-02-16 18:53:06 +0300

    lsp: lspkind, templ

--- nvim/.config/nvim/lua/pio/plugin/lsp.lua
+++ nvim/.config/nvim/lua/pio/plugin/lsp.lua
@@ -111,6 +111,7 @@ return {
                 bashls = {},
                 ts_ls = {},
                 sqlls = {},
+                templ = {},
             }
 
             -- Let Mason install the servers, check with :Mason
@@ -209,6 +210,57 @@ return {
             local luasnip = require 'luasnip'
             luasnip.config.setup {}
 
+            local lspkind = require 'lspkind'
+            lspkind.init {
+                -- DEPRECATED (use mode instead): enables text annotations
+                --
+                -- default: true
+                -- with_text = true,
+
+                -- defines how annotations are shown
+                -- default: symbol
+                -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+                mode = 'symbol_text',
+
+                -- default symbol map
+                -- can be either 'default' (requires nerd-fonts font) or
+                -- 'codicons' for codicon preset (requires vscode-codicons font)
+                --
+                -- default: 'default'
+                preset = 'codicons',
+
+                -- override preset symbols
+                --
+                -- default: {}
+                symbol_map = {
+                    Text = '󰉿',
+                    Method = '󰆧',
+                    Function = '󰊕',
+                    Constructor = '',
+                    Field = '󰜢',
+                    Variable = '󰀫',
+                    Class = '󰠱',
+                    Interface = '',
+                    Module = '',
+                    Property = '󰜢',
+                    Unit = '󰑭',
+                    Value = '󰎠',
+                    Enum = '',
+                    Keyword = '󰌋',
+                    Snippet = '',
+                    Color = '󰏘',
+                    File = '󰈙',
+                    Reference = '󰈇',
+                    Folder = '󰉋',
+                    EnumMember = '',
+                    Constant = '󰏿',
+                    Struct = '󰙅',
+                    Event = '',
+                    Operator = '󰆕',
+                    TypeParameter = '',
+                },
+            }
+
             cmp.setup {
                 snippet = {
                     expand = function(args)
@@ -279,6 +331,27 @@ return {
                     { name = 'luasnip' },
                     { name = 'path' },
                 },
+                formatting = {
+                    format = lspkind.cmp_format {
+                        mode = 'symbol', -- show only symbol annotations
+                        maxwidth = {
+                            -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                            -- can also be a function to dynamically calculate max width such as
+                            -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+                            menu = 50, -- leading text (labelDetails)
+                            abbr = 50, -- actual suggestion item
+                        },
+                        ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                        show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+                        -- The function below will be called before any actual modifications from lspkind
+                        -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+                        before = function(entry, vim_item)
+                            -- ...
+                            return vim_item
+                        end,
+                    },
+                },
             }
         end,
     },
@@ -327,5 +400,13 @@ return {
         'mrcjkb/rustaceanvim',
         version = '^5', -- Recommended
         lazy = false, -- This plugin is already lazy
+    },
+    {
+        'onsails/lspkind-nvim',
+        config = function() end,
+        event = 'InsertEnter',
+        dependencies = {
+            'hrsh7th/nvim-cmp',
+        },
     },
 }
