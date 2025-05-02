6cf724075d7d9ec88a7deb1a398d648bdd4fa611
xinoip <xinoip@proton.me>
2025-05-02 23:19:21 +0300

    nvim: gopher.nvim

--- nvim/.config/nvim/lua/pio/plugin/language/go.lua
+++ nvim/.config/nvim/lua/pio/plugin/language/go.lua
@@ -1,20 +1,35 @@
 return {
-    'fredrikaverpil/godoc.nvim',
-    version = '*',
-    dependencies = {
-        { 'folke/snacks.nvim' }, -- optional
-        { 'ibhagwan/fzf-lua' }, -- optional
-        {
-            'nvim-treesitter/nvim-treesitter',
-            opts = {
-                ensure_installed = { 'go' },
+    {
+        'fredrikaverpil/godoc.nvim',
+        version = '*',
+        dependencies = {
+            { 'folke/snacks.nvim' }, -- optional
+            { 'ibhagwan/fzf-lua' }, -- optional
+            {
+                'nvim-treesitter/nvim-treesitter',
+                opts = {
+                    ensure_installed = { 'go' },
+                },
+            },
+        },
+        cmd = { 'GoDoc' }, -- optional
+        opts = {
+            picker = {
+                type = 'snacks',
+            },
+        }, -- see further down below for configuration
+    },
+    {
+        'olexsmir/gopher.nvim',
+        ft = 'go',
+        -- build = function()
+        --     vim.cmd.GoInstallDeps()
+        -- end,
+        ---@type gopher.Config
+        opts = {
+            gotag = {
+                transform = 'camelcase',
             },
         },
     },
-    cmd = { 'GoDoc' }, -- optional
-    opts = {
-        picker = {
-            type = 'snacks',
-        },
-    }, -- see further down below for configuration
 }
