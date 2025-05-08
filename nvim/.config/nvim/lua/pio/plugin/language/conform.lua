09604cb3a7272325bc34cc6ae669f202e4aaaaff
xinoip <xinoip@proton.me>
2025-05-08 21:22:12 +0300

    nvim: js/ts formatters

--- nvim/.config/nvim/lua/pio/plugin/language/conform.lua
+++ nvim/.config/nvim/lua/pio/plugin/language/conform.lua
@@ -37,6 +37,10 @@ return {
         formatters_by_ft = {
             lua = { 'stylua' },
             cmake = { 'cmake_format' },
+            javascript = { 'prettierd', 'prettier', stop_after_first = true },
+            javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+            typescript = { 'prettierd', 'prettier', stop_after_first = true },
+            typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         },
     },
 }
