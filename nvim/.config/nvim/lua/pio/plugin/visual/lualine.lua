66fb18e713decbbb14ebd5c8262c1f46dd358884
xinoip <xinoip@proton.me>
2025-07-16 13:43:07 +0300

    nvim: oled colors

--- nvim/.config/nvim/lua/pio/plugin/visual/lualine.lua
+++ nvim/.config/nvim/lua/pio/plugin/visual/lualine.lua
@@ -2,9 +2,13 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
+        local custom_icerberg_dark = require 'lualine.themes.iceberg_dark'
+        custom_icerberg_dark.normal.c.bg = '#000000'
+
         require('lualine').setup {
             options = {
-                theme = 'gruvbox-material',
+                globalstatus = true,
+                theme = custom_icerberg_dark,
             },
         }
     end,
