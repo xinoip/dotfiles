a24460567a5660b39e3770eddf3991e9630284e3
xinoip <xinoip@proton.me>
2025-08-28 01:40:56 +0300

    nvim: fix markview

--- nvim/.config/nvim/lua/pio/plugin/language/markdown.lua
+++ nvim/.config/nvim/lua/pio/plugin/language/markdown.lua
@@ -16,13 +16,14 @@ return {
     {
         'OXY2DEV/markview.nvim',
         lazy = false, -- Recommended
+        priority = 49,
         -- ft = "markdown" -- If you decide to lazy-load anyway
 
         dependencies = {
             -- You will not need this if you installed the
             -- parsers manually
             -- Or if the parsers are in your $RUNTIMEPATH
-            'nvim-treesitter/nvim-treesitter',
+            -- 'nvim-treesitter/nvim-treesitter',
 
             'nvim-tree/nvim-web-devicons',
         },
