d534031a1d170f4eba6275e2c79c826d8cc9c2c2
xinoip <xinoip@proton.me>
2025-12-20 19:01:07 +0300

    nvim: disable lazydev from blink

--- nvim/.config/nvim/lua/pio/plugin/lsp/blink.lua
+++ nvim/.config/nvim/lua/pio/plugin/lsp/blink.lua
@@ -13,16 +13,18 @@ return {
         },
         -- TODO: check this
         completion = { documentation = { auto_show = true } },
+        -- TODO: lazydev not working
         sources = {
-            default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+            -- default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+            default = { "lsp", "path", "snippets", "buffer" },
         },
-        providers = {
-            lazydev = {
-                name = "LazyDev",
-                module = "lazydev.integrations.blink",
-                score_offset = 100,
-            },
-        },
+        -- providers = {
+        --     lazydev = {
+        --         name = "LazyDev",
+        --         module = "lazydev.integrations.blink",
+        --         score_offset = 100,
+        --     },
+        -- },
         fuzzy = { implementation = "prefer_rust_with_warning" },
     },
     opts_extend = { "sources.default" },
