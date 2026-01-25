19b6da85700a9f9ae397a013823a6201beb1b698
xinoip <xinoip@proton.me>
2026-01-25 16:13:52 +0300

    nvim: conform ignore html

--- nvim/.config/nvim/lua/pio/plugin/lsp/conform.lua
+++ nvim/.config/nvim/lua/pio/plugin/lsp/conform.lua
@@ -19,7 +19,7 @@ return {
         notify_on_error = false,
         format_on_save = function(bufnr)
             -- Disable autoformat on certain filetypes
-            local ignore_filetypes = { "java" }
+            local ignore_filetypes = { "java", "html" }
             if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
                 return
             end
