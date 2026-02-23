a2883e6fa1c3af1fc81edc3b20695a4c986f4bc4
xinoip <xinoip@proton.me>
2026-02-23 16:52:14 +0300

    nvim: change avante model

--- nvim/.config/nvim/lua/pio/plugin/ai/avante.lua
+++ nvim/.config/nvim/lua/pio/plugin/ai/avante.lua
@@ -6,6 +6,11 @@ return {
     opts = {
         instructions_file = "AGENTS.md",
         provider = "copilot",
+        providers = {
+            copilot = {
+                model = "gpt-5-mini",
+            },
+        },
         behaviour = {
             use_cwd_as_project_root = true,
         },
