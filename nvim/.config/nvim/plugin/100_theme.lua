372c3f2510c09e777ce1320c234eb797bda2e8a2
xinoip <xinoip@proton.me>
2026-04-14 20:13:22 +0300

    nvim: fix dark/light

--- nvim/.config/nvim/plugin/100_theme.lua
+++ nvim/.config/nvim/plugin/100_theme.lua
@@ -2,7 +2,9 @@ local koda_opts = {}
 local lualine_theme = require("lualine.themes.auto")
 
 if vim.o.background == "dark" then
-    koda_opts.colors.bg = "#000000"
+    koda_opts.colors = {
+        bg = "#000000",
+    }
     lualine_theme.normal.c.bg = "#000000"
 else
     lualine_theme.normal.c.bg = "#ebebeb"
