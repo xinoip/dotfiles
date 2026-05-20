beb4d4d1c0621366c90950e54227329528102c4f
xinoip <xinoip@proton.me>
2026-05-21 01:57:59 +0300

    zsh: Fix Lazygit GPG bug workaround.

--- zsh/.config/zsh/aliases/programs.zsh
+++ zsh/.config/zsh/aliases/programs.zsh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 
-alias lg='lazygit'
+# alias lg='lazygit'
+# GPG Password flow breaks Lazygit.
+alias lg='echo "foo" | gpg --sign > /dev/null; lazygit'
 alias lzd='lazydocker'
 alias lf='yzcd'
 alias du='dust'
